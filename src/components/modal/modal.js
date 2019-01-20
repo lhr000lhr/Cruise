c26a469a4d19c82f3cf72e4cdd0bcb1b4b356230
Mac <mac@MacdeMacBook-Pro.local>
2019-01-21 04:12:41 +0800

    feat: add modal, modify resource-card

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,25 +1,57 @@
+/** 
+ * Modal 
+ * 
+ * fn:        function
+ * top:       number | string
+ * left:      number | string
+ * 
+ * */
+
 export default class Modal {
 
   constructor(options) {
-    this.options = options;
-    
+    this.defoptions = {};
+    this.options = Object.assign(this.defoptions, options);
+
   }
 
   createElement() {
     this.el = document.createElement('div');
     this.el.className = 'modal';
+    this.el.style.top = `${this.options.top + 26}px`;
+    this.el.style.left = `${this.options.left - 20}px`;
     this.el.innerHTML = this.createTemplate();
     return this.el;
   }
 
   createTemplate() {
     return `
-      
+    <div class="modal-content">
+      <p class="modal-title">
+        Sepatate multiple resource name with commas
+      </p>
+      <div class="modal-input">
+        <input type="text">
+      </div>
+      <div class="modal-operation">
+        <button class="button-pramry modal-confirm">
+          Add Resource
+        </button>
+        <button class="modal-cancel">
+          Cancel
+        </button>
+      </div>
+    </div>
+    <i class="cruise icon-close modal-close"></i>
     `
   }
 
   confirm() {
-    this.options.fn();
+    const inputEl = this.el.querySelector('.modal-input input');
+    const value = inputEl.value;
+    if (value) {
+      this.options.fn(value);
+    }
     this.destory();
   }
 
@@ -27,9 +59,16 @@ export default class Modal {
     this.destory();
   }
 
+  close() {
+    this.destory();
+  }
+
   destory() {
     const bodyEl = document.getElementById('app-root');
+    window.onmousewheel = document.onmousewheel = null;
+    document.removeEventListener('DOMMouseScroll', this.scrollFunc);
     bodyEl.removeChild(this.el);
+
   }
 
   clear() {
@@ -41,10 +80,47 @@ export default class Modal {
     }
   }
 
+  createCancelListener() {
+    const cancelBtn = this.el.querySelector('.modal-cancel');
+    cancelBtn.addEventListener('click', this.cancel.bind(this), false);
+  }
+
+  createCloseListener() {
+    const closeBtn = this.el.querySelector('.modal-close');
+    closeBtn.addEventListener('click', this.close.bind(this), false);
+  }
+
+  createConfirmListener() {
+    const confirmBtn = this.el.querySelector('.modal-confirm');
+    confirmBtn.addEventListener('click', this.confirm.bind(this), false);
+  }
+
+  createSrollListener() {
+    if (document.addEventListener) { // firefox  
+      document.addEventListener('DOMMouseScroll', this.scrollFunc.bind(this), false);
+    }
+    // ie chrome 
+    window.onmousewheel = document.onmousewheel = this.scrollFunc.bind(this);
+  }
+
+  scrollFunc (e) {
+    e = e || window.event;
+    if (e.wheelDelta) { // ie chrome           
+      this.destory();
+    } else if (e.detail) { // firefox
+      this.destory();
+    }
+  }
+
+
   init() {
     this.clear();
     const curModal = this.createElement();
     const bodyEl = document.getElementById('app-root');
     bodyEl.appendChild(curModal);
+    this.createConfirmListener();
+    this.createCancelListener();
+    this.createCloseListener();
+    this.createSrollListener();
   }
 }
--- src/components/resource/resource-card.js
+++ src/components/resource/resource-card.js
@@ -12,6 +12,8 @@
  * 
  * */
 
+ import Modal from '../modal/modal';
+
 
 export default class ResourceCard {
 
@@ -32,89 +34,109 @@ export default class ResourceCard {
   createTemplate() {
     const imgUrl = require(`../../assets/os-icons/${this.options.os}.png`);
     return `
-      <div class="resource-card-icon-wrap">
-        <img src="${imgUrl}" alt="">
+  
+  <div class="resource-card-icon-wrap">
+  <img src="${imgUrl}" alt="">
+</div>
+<div class="resource-card-article">
+  <div class="row resource-card-description">
+    <div class="col-5 resource-name text-overflow">
+      <i class="cruise icon-desktop"></i>
+      <span>${this.options.name}</span>
+    </div>
+    <div class="col-1 resource-status resource-status-building">
+    <span>${this.options.status}</span>
+    </div>
+    <div class="col-3 resource-ip text-overflow">
+    <span>${this.options.ip}</span>
+    </div>
+    <div class="col-3 resource-location text-overflow">
+    <span>${this.options.location}</span>
+    </div>
+  </div>
+  <div class="row resource-card-operation">
+    <div class="col-10">
+      <div class="resource-browser clearfix">
+        <span class="icon-plus-wrap float-left">
+          <i class="cruise icon-plus"></i>
+        </span>
+        <p class="float-left resource-browser-list brower-wrap"></p>
       </div>
-      <div class="resource-card-article">
-        <div class="row resource-card-description">
-          <div class="col-5">
-            <i class="cruise icon-desktop"></i>
-            <span>${this.options.name}</span>
-          </div>
-          <div class="col-1">
-            <span>${this.options.status}</span>
-          </div>
-          <div class="col-3">
-              <span>${this.options.ip}</span>
-          </div>
-          <div class="col-3">
-              <span>${this.options.location}</span>
-          </div>
-        </div>
-        <div class="row resource-card-operation">
-          
-          <div class="col-10">
-              <span class="">
-                <i class="cruise icon-plus"></i>
-              </span>
-              <p class="brower-wrap"></p>
-          </div>
-          ${this.options.status === 'building' ? 
+    </div>
+    <div class="col-2 deny-button clearfix">
+    ${this.options.status === 'building' ? 
             `
-              <div class="col-2">
-                <button type="button float-right">
-                  Deny
-                </button>
-              </div>
+            <button type="button">
+            <i class="cruise icon-deny"></i>
+            Deny
+          </button>
             ` :
             ''
           }
-          
-        </div>
-      </div>
+      
+    </div>
+  </div>
+</div>
     `
   }
 
   refreshBrowerWrapElement(list) {
-    if (!list || !list.length) return;
+    if (!list || !list.length) list = [];
     this.browerWrapEl = this.el.querySelector('.brower-wrap');
+    this.browerWrapEl.innerHTML = '';
     for (let i = 0; i < list.length; i++) {
       const browerLabelEl = this.createBrowerLabelElement(list[i]);
       this.browerWrapEl.appendChild(browerLabelEl);
     }
     this.addListenerToBrowerLabel();
+    this.addListenerToAddBtn();
   }
 
   createBrowerLabelElement(name) {
     const browerLabelEl = document.createElement('span');
-    browerLabelEl.className = 'brower-label';
+    browerLabelEl.className = 'resource-browser-label';
     browerLabelEl.innerHTML = `
-      <span>${name} <i class="cruise icon-trash"></i></span>
+      ${name} <i class="cruise icon-trash"></i>
     `;
     return browerLabelEl;
   }
 
-  addBrowserTrigger(name) {
-    this.options.resources = this.options.resources || [];
-    this.options.resources.push(name);
-
+  removeBrowser(ev) {
+    var ev = ev || window.event;
+    var target = ev.target || ev.srcElement;
+    if (target.nodeName.toLowerCase() == 'i') {
+      const grandparent = target.parentNode.parentNode;
+      const parent = target.parentNode;
+      grandparent.removeChild(parent);
+    }
   }
 
-  removeBrowser(target) {
-    if (!this.options.resources || !this.options.resources.length) return;
-    const grandparent = target.parentNode.parentNode;
-    const parent = target.parentNode;
-    grandparent.removeChild(parent);
+  createModal(ev) {
+    const addBtn = this.el.querySelector('.icon-plus');
+    const oRect = addBtn.getBoundingClientRect();
+    const top = oRect.top;
+    const left = oRect.left;
+    const options = {
+      fn: (name) => {
+        this.options.resources = this.options.resources || [];
+        const browerLabelEl = this.createBrowerLabelElement(name);
+        this.browerWrapEl.appendChild(browerLabelEl);
+      },
+      top,
+      left
+    }
+    const modalRef = new Modal(options);
+    modalRef.init();
   }
 
   addListenerToBrowerLabel() {
-    this.browerWrapEl.addEventListener('click', (ev) => {
-      var ev = ev || window.event;
-      var target = ev.target || ev.srcElement;
-      if (target.nodeName.toLowerCase() == 'i') {
-        this.removeBrowser(target);
-      }
-    }) 
+    this.browerWrapEl.addEventListener('click', this.removeBrowser.bind(this))
+  }
+
+  addListenerToAddBtn() {
+    const addBtn = this.el.querySelector('.icon-plus');
+    addBtn.addEventListener('click', this.createModal.bind(this));
+    
   }
 
   init() {
